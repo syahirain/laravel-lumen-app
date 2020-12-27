444b45d0a8888611ef3757f2ea1c0256ba0853da
AHMAD SYAHIRAN <syahirain@users.noreply.github.com>
2020-12-28 00:54:50 +0800

    error handling

--- timerAPI/app/Http/Controllers/TimerController.php
+++ timerAPI/app/Http/Controllers/TimerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Timer;
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
 
 class TimerController extends Controller
 {
@@ -11,20 +12,48 @@ class TimerController extends Controller
     {
         $timer = $request->input();
           $a=array();
+          $count=$leftover=0;
           for($i=0;$i<$timer['unique_code'];$i++)
           {
-             $str = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
-            // $a['unique_code'] = substr(str_shuffle($str), 0, 7);
-            // Timer::insert($a);
+            $str = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+          //  $str = '123';
             array_push($a,array('unique_code'=> substr(str_shuffle($str), 0, 7)));
           }
-          foreach (array_chunk($a,50000) as $t)  
+
+
+          while($count < $timer['unique_code'])
+          {
+            if($count > 0)
             {
-                //DB::table('table_name')->insert($t); 
-                Timer::insert($t);
+              $leftover = $timer['unique_code'] - $count; // get leftover unique code needed to do
+              $a=array(); // clear out old array
             }
-          //Timer::insert($a);
-        return response()->json('DONE QUERY', 201);
+
+            for($i=0;$i<$leftover;$i++)
+            {
+              $str = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+              array_push($a,array('unique_code'=> substr(str_shuffle($str), 0, 7)));
+            }
+
+            foreach (array_chunk($a,10000) as $t)  
+              {
+                  try {
+                      // Validate the value..
+                      Timer::insert($t);
+                      $count += count($t);
+                  } catch (QueryException $e){
+                    $errorCode = $e->errorInfo[1];
+                    if($errorCode == 1062){
+                      continue;  
+                    }else{
+                      return response()->json($errorCode);
+                    }
+                    
+                  }
+              }
+          }
+          
+        return response()->json($count);
         //return response()->json($timer);
         //return var_dump($timer);
        //return $timer;
